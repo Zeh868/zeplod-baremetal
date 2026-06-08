1f0b22e144a3bfe5f9bc0944f9458fecf5c6b6ee
硬核嵌入式 <china_qzh@163.com>
2026-06-08 12:00:45 +0800

    test(core): add mempool unit tests

--- src/core/bm_critical.c
+++ src/core/bm_critical.c
@@ -3,13 +3,19 @@
 /* Default implementation for native_sim: use a global flag */
 static volatile bm_irq_state_t _irq_state = 0;
 
-__attribute__((weak)) bm_irq_state_t bm_hal_critical_enter(void) {
+#if defined(_MSC_VER)
+#define BM_WEAK
+#else
+#define BM_WEAK __attribute__((weak))
+#endif
+
+BM_WEAK bm_irq_state_t bm_hal_critical_enter(void) {
     bm_irq_state_t prev = _irq_state;
     _irq_state = 1;
     return prev;
 }
 
-__attribute__((weak)) void bm_hal_critical_exit(bm_irq_state_t state) {
+BM_WEAK void bm_hal_critical_exit(bm_irq_state_t state) {
     _irq_state = state;
 }
 
---
+++ tests/unit/test_mempool.c
@@ -0,0 +1,36 @@
+#include "unity.h"
+#include "bm_core.h"
+
+typedef struct { uint32_t a; uint8_t b; } test_obj_t;
+
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_mempool_alloc_free(void) {
+    BM_MEMPOOL_DEFINE(pool, test_obj_t, 4);
+    memset(pool.bitmap, 0, pool.bitmap_words * sizeof(uint32_t));
+
+    test_obj_t *a = (test_obj_t *)bm_mempool_alloc(&pool);
+    TEST_ASSERT_NOT_NULL(a);
+    a->a = 123;
+
+    bm_mempool_free(&pool, a);
+    test_obj_t *b = (test_obj_t *)bm_mempool_alloc(&pool);
+    TEST_ASSERT_EQUAL_PTR(a, b);
+}
+
+void test_mempool_exhausted(void) {
+    BM_MEMPOOL_DEFINE(pool, test_obj_t, 2);
+    memset(pool.bitmap, 0, pool.bitmap_words * sizeof(uint32_t));
+
+    TEST_ASSERT_NOT_NULL(bm_mempool_alloc(&pool));
+    TEST_ASSERT_NOT_NULL(bm_mempool_alloc(&pool));
+    TEST_ASSERT_NULL(bm_mempool_alloc(&pool));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_mempool_alloc_free);
+    RUN_TEST(test_mempool_exhausted);
+    return UNITY_END();
+}
