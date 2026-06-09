87b8d44b7d7a1fe4ee59bd2ff7c9d857b6a5d8a2
硬核嵌入式 <china_qzh@163.com>
2026-06-09 21:07:33 +0800

    feat(examples): add full_system config

---
+++ examples/full_system/bm_config.h
@@ -0,0 +1,12 @@
+#ifndef BM_CONFIG_H
+#define BM_CONFIG_H
+
+#define BM_CONFIG_MAX_EVENT_TYPES           16
+#define BM_CONFIG_MAX_EVENT_SUBSCRIBERS     32
+#define BM_CONFIG_EVENT_QUEUE_SIZE          32
+#define BM_CONFIG_EVENT_PRIORITIES          4
+#define BM_CONFIG_MAX_MODULES               8
+#define BM_CONFIG_MEMPOOL_MAX_POOLS         4
+#define BM_CONFIG_MAX_WDG_MODULES           4
+
+#endif
